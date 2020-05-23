d789a56078543a48710178c192b703342efe1bc9
yuichiro-cloud <ghjhbc1@gmail.com>
2020-05-24 02:27:14 +0900

    agc44 a 不正解

---
+++ agc43/a.py
@@ -0,0 +1,14 @@
+# h,w = map(int,input().split())
+
+# s = []
+
+# for i in range(h):
+#   s.append(list((map(int,input()))))
+# print(s)
+
+# for i in range(h):
+#   if s[0][0] == '#':
+
+#   elif s[i][]
+
+
---
+++ agc44/a.py
@@ -0,0 +1,31 @@
+import math
+# num = 0
+# coin = 0
+
+t = int(input())
+
+# n = []
+# a = []
+# b = []
+# c = []
+# d = []
+l = []
+for i in range(t):
+  l.append((list(map(int,input().split()))))
+  # n.append(l[0])
+  # a.append(l[1])
+  # b.append(l[2])
+  # c.append(l[3])
+  # d.append(l[4])
+# print(l[0][0])
+
+for i in range(t):
+  r2 = 0
+  # print(l[t][0])
+  while 5**r2 <= l[i][0]:
+    5**r2
+    r2 += 1
+
+# print(r2)
+
+
