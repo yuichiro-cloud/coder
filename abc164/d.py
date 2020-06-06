f1266a70a48a592ebeaaee9713a4596e5e79245c
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-06 22:12:08 +0900

    d.py timeover by using numpy

--- abc164/d.py
+++ abc164/d.py
@@ -1,9 +1,13 @@
+import numpy as np
 s = input()
 len_s = len(s)
-count = 0
+counter = []
 for i in range(len_s + 1):
   for index in range(i + 1):
-    if i != index and int(s[index:i])%2019 == 0:
-      count+=1
-print(count)
+    if i != index:
+      counter.append(int(s[index:i]))
+np_counter = np.array(counter)
+np_2019 = np_counter % 2019
+print(np.count_nonzero(np_2019 == 0))
+
 
