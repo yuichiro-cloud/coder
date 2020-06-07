015671bf26de6cfa581715652102f3911e6240bc
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-07 21:24:21 +0900

    commit

--- abc164/d.py
+++ abc164/d.py
@@ -10,4 +10,11 @@ np_counter = np.array(counter)
 np_2019 = np_counter % 2019
 print(np.count_nonzero(np_2019 == 0))
 
-
+# s = input()
+# len_s = len(s)
+# count = 0
+# for i in range(len_s + 1):
+#   for index in range(i + 1):
+#     if i != index and int(s[index:i])%2019 == 0:
+#       count+=1
+# print(count)
