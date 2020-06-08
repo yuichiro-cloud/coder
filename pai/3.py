bb2245f6bde715ff62125ec76155579db8e28932
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-08 15:31:27 +0900

    success

--- pai/3.py
+++ pai/3.py
@@ -1,24 +1,14 @@
-import math
 p1,p2,p3,k = map(int,input().split())
 
 k_box = []
-tem = []
-num = 1
-while len(k_box) <k:
-  index2 = int(math.log(num,2)//1)
-  index3 = int(math.log(num,3)//1)
-  index5 = int(math.log(num,5))
-  if num in tem:
-    k_box.append(num)
-  else:
-    for i in range(index2+1):
-      for i2 in range(index3+1):
-        for i3 in range(index5+1):
-          tem.append((p1**i)*(p2**i2)*(p3**i3))
-    if num in tem:
-      k_box.append(num)
-    set(tem)
-  num+=1
+index2 = 40
+index3 = 30
+index5 = 25
+for i in range(index2):
+  for i2 in range(index3):
+    for i3 in range(index5):
+      k_box.append(p1**(i)*p2**(i2)*p3**(i3))
+k_box.sort()
 print(k_box[k-1])
 
 
