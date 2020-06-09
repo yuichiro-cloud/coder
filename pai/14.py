f50f0defea7be4f5463d4f58fe20346618a54826
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-09 13:05:15 +0900

    c

---
+++ pai/14.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(1,n+1):
+  if i %15 == 0:
+    print('Fizz Buzz')
+  elif i % 3 == 0:
+    print('Fizz')
+  elif i % 5 == 0:
+    print('Buzz')
+  else:
+    print(i)
