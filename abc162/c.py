e9bb4070aac78f22e1b830b7ba8d14efd1695949
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-11 20:56:41 +0900

    commit

--- abc162/c.py
+++ abc162/c.py
@@ -1,10 +1,15 @@
 import math
+import time
 from functools import reduce
 k = int(input())
+t1 = time.time()
 
-l =0
+l = 0
 for i in range(1,k+1):
   for i2 in range(1,k+1):
     for i3 in range(1,k+1):
-      l+=reduce(math.gcd,[i,i2,i3])
+      # l+=reduce(math.gcd,[i,i2,i3])
+      l+=i3
 print(l)
+t2 = time.time()
+print(t2 - t1)
