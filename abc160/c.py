7c17c174e9df1ab24067e3c76eb80e1aefd078e0
yuichiro-cloud <ghjhbc1@gmail.com>
2020-06-13 15:33:48 +0900

    c f

--- abc160/c.py
+++ abc160/c.py
@@ -4,8 +4,8 @@ a = list(map(int,input().split()))
 count = []
 
 for i in range(n):
-  if (a[i-1]-a[i])<0:
-    count.append(a[i-1]-a[i]+20)
+  if a[i]-a[i-1]<0:
+    count.append(20-(a[i]-a[i-1]+20))
   else:
-    count.append(a[i-1]-a[i])
+    count.append(20-(a[i]-a[i-1]))
 print(min(count))
