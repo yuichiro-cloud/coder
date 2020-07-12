8da69dcf9bd18df04e704180b870ae025c6eb6c8
yuichiro-cloud <ghjhbc1@gmail.com>
2020-07-12 12:29:02 +0900

    abc157 c
    success

--- abc157/c.py
+++ abc157/c.py
@@ -1,42 +1,55 @@
 n,m = map(int,input().split())
-sc = []
+c1 = []
+c2 = []
+c3 = []
 for i in range(m):
-  sc.append(list(map(int,input().split())))
-
-sc1 = []
-sc2 = []
-sc3 = []
-for i in sc:
-  if i[0]==1:
-    sc1.append(i[1])
-  elif i[0]==2:
-    sc2.append(i[1])
+  sc = list(map(int,input().split()))
+  if sc[0] == 1:
+    c1.append(sc[1])
+  elif sc[0] == 2:
+    c2.append(sc[1])
   else:
-    sc3.append(i[1])
+    c3.append(sc[1])
 
-if len(set(sc1)) > 1 or len(set(sc2)) > 1 or len(set(sc3)) > 1:
+if len(set(c1)) <= 1 and len(set(c2)) <= 1 and len(set(c3)) <= 1:
+  if 0 in list(set(c1)):
+    if n == 1 and len(set(c1)) == 1 and len(set(c2)) == 0 and len(set(c3)) == 0:
+      print('0')
+      exit()
+    else:
+      print('-1')
+      exit()
+  elif not c1 and not c2 and not c3:
+    if n == 1:
+      print('0')
+      exit()
+    elif n == 2:
+      print('10')
+      exit()
+    else:
+      print('100')
+      exit()
+  else:
+    if list(set(c1)):
+      ans_c1 = list(set(c1))[0]
+    else:
+      ans_c1 = 1
+    if list(set(c2)):
+      ans_c2 = list(set(c2))[0]
+    else:
+      ans_c2 = 0
+    if list(set(c3)):
+      ans_c3 = list(set(c3))[0]
+    else:
+      ans_c3 = 0
+else:
   print('-1')
   exit()
-elif 0 in list((set(sc1))):
-  print('-1')
-  exit()
 
-
-c1 = list(set(sc1))
-c2 = list(set(sc2))
-c3 = list(set(sc3))
-
-if c1:
-  c1_ans = c1[0]
+if n == 1:
+  print(ans_c1)
+elif n == 2:
+  print(ans_c1*10 + ans_c2)
 else:
-  c1_ans = 1
-if c2:
-  c2_ans = c2[0]
-else:
-  c2_ans = 0
-if c3:
-  c3_ans = c3[0]
-else:
-  c3_ans = 0
+  print(ans_c1*100 + ans_c2*10 + ans_c3)
 
-print(100*c1_ans + 100*c2_ans + c3_ans)
